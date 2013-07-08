2fb34b1a937c335f126588632f9f9b8c6beca1d3
Nik K <nik.karbaum@gmail.com>
2013-07-08 16:16:32 +0200

    Changed the default from Debug to Release

--- app/ArmagetronProtocol/Form1.cs
+++ app/ArmagetronProtocol/Form1.cs
@@ -25,29 +25,26 @@ namespace ArmagetronProtocol
             if (this.done)
                 Application.Exit();
 
-            RegistryKey key = Registry.ClassesRoot.OpenSubKey("armagetronad");  //open myApp protocol's subkey
+            RegistryKey key = Registry.ClassesRoot.OpenSubKey("armagetronad");  
 
-            if (key == null)  //if the protocol is not registered yet...we register it
-            {
-                key = Registry.ClassesRoot.CreateSubKey("armagetronad");
-                key.SetValue(string.Empty, "URL: ArmagetronAd Protocol");
-                key.SetValue("URL Protocol", string.Empty);
+            key = Registry.ClassesRoot.CreateSubKey("armagetronad");
+            key.SetValue(string.Empty, "URL: ArmagetronAd Protocol");
+            key.SetValue("URL Protocol", string.Empty);
 
-                key = key.CreateSubKey(@"shell\open\command");
-                RegistryKey key2 = key.CreateSubKey(@"DefaultIcon");
+            key = key.CreateSubKey(@"shell\open\command");
+            RegistryKey key2 = key.CreateSubKey(@"DefaultIcon");
 
-                String path = string.Empty;
-                OpenFileDialog dialog = new OpenFileDialog();
-                dialog.Filter = "executables|*.exe";
-                dialog.InitialDirectory = "C:";
-                dialog.Title = "Select the Armagetron Executable";
+            String path = string.Empty;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "executables|*.exe";
+            dialog.InitialDirectory = "C:";
+            dialog.Title = "Select the Armagetron Executable";
 
-                if (dialog.ShowDialog() == DialogResult.OK)
-                    path = dialog.FileName;
+            if (dialog.ShowDialog() == DialogResult.OK)
+                path = dialog.FileName;
 
-                key.SetValue(string.Empty, path + " --connect " + "%1");
-                key2.SetValue(string.Empty, path);
-            }
+            key.SetValue(string.Empty, path + " --connect " + "%1");
+            key2.SetValue(string.Empty, path);
 
             button1.Text = "Done!";
 
